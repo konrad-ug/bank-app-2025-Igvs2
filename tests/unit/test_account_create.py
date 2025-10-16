fe4aa4450f59b2a83111665bb03051b8a50fdde9
Igor Bieńczak <ibienczak@inf.ug.edu.pl>
2025-10-16 13:16:03 +0200

    feature2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", 0)
+        account = Account("John", "Doe", "123132", 0)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
+        assert account.pesel == "123132"

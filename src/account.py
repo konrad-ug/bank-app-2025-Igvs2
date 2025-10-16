5ee20ce86a6c5bc5dc80f316ba55ac1b3589e412
Igor Bieńczak <ibienczak@INF.UG.EDU.PL>
2025-10-16 13:39:58 +0200

    feature4

--- src/account.py
+++ src/account.py
@@ -1,8 +1,12 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel, balance = 0):
+    def __init__(self, first_name, last_name, pesel, balance = 0, promo_code = None):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = balance
+        self.promo_code = promo_code
+
+        if promo_code and promo_code.startswith("promo_"):
+            self.balance = balance + 50
 
         if len(pesel) != 11:
             self.pesel = "Invalid"
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,7 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation_valid_pesel(self):
-        account = Account("John", "Doe", "12345678901", 0)
+        account = Account("John", "Doe", "12345678901", 0,)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
@@ -15,3 +15,17 @@ class TestAccount:
         assert account.last_name == "Smith"
         assert account.balance == 0
         assert account.pesel == "Invalid"
+
+    def test_promo_code_valid(self):
+        account = Account("Jane", "Smith", "12345", 0, "promo_123")
+        assert account.balance == 50
+
+
+    def test_promo_code_invalid(self):
+        account = Account("Jane", "Smith", "12345", 0, "promo213")
+        assert account.balance == 0
+
+    def test_promo_code_none(self):
+        account = Account("Jane", "Smith", "12345", 0)
+        assert account.balance == 0
+
